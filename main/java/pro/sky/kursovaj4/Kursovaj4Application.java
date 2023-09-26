0fcd4a5a8eb0440d2e22a8bcc836457611b98c7e
Kinserega <kindratsergej5@gmail.com>
2023-09-26 13:10:58 +0300

    Задача

---
+++ main/java/pro/sky/kursovaj4/Kursovaj4Application.java
@@ -0,0 +1,13 @@
+package pro.sky.kursovaj4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Kursovaj4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Kursovaj4Application.class, args);
+	}
+
+}
---
+++ test/java/pro/sky/kursovaj4/Kursovaj4ApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.kursovaj4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Kursovaj4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
